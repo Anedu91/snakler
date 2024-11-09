b8051a8b62bf2c33c6962cf3fc5b1520cb375dad
Angel Eduardo <40308240+Anedu91@users.noreply.github.com>
2024-11-09 18:45:40 -0300

    initial socket and folders

---
+++ server/src/chat/__init__.py
@@ -0,0 +1,33 @@
+
+from quart import Quart, render_template, websocket
+import asyncio
+
+from chat.broker import Broker
+
+app = Quart(__name__)
+
+
+broker = Broker()
+
+async def _receive() -> None:
+    while True:
+        message = await websocket.receive()
+        await broker.publish(message)
+
+
+@app.get("/")
+async def index():
+    return await render_template("index.html")
+
+@app.websocket("/ws")
+async def ws() -> None:
+    try:
+        task = asyncio.ensure_future(_receive())
+        async for message in broker.subscribe():
+            await websocket.send(message)
+    finally:
+        task.cancel()
+        await task
+
+def run() -> None:
+    app.run()
