c5eda008a6f1a10fcfc780964cfbf2615ec7110f
Angel Eduardo <40308240+Anedu91@users.noreply.github.com>
2024-11-18 20:50:41 -0300

    elm config for moving arrow

--- front/src/Main.elm
+++ front/src/Main.elm
@@ -1,126 +1,53 @@
-port module Main exposing (main)
-
-
+module Main exposing (main)
 import Browser
+import Browser.Events
+
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Json.Decode as D
+import Json.Decode
 
 
-
--- MAIN
-
-
-main : Program () Model Msg
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
-
-
-
-
--- PORTS
-
-
-port sendMessage : String -> Cmd msg
-port messageReceiver : (String -> msg) -> Sub msg
-
-
-
--- MODEL
-
-
-type alias Model =
-  { draft : String
-  , messages : List String
+  Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
+type alias Model = {
+  snakePosition: List (Int, Int),
+  area: Int,
+  direction: Direction
   }
 
-
-init : () -> ( Model, Cmd Msg )
-init flags =
-  ( { draft = "", messages = [] }
-  , Cmd.none
-  )
+type Direction = Up | Down | Left | Right
 
 
 
--- UPDATE
+init: () -> (Model, Cmd Msg)
+init _ = ( { snakePosition = [(0,0)], area = 500, direction = Up }, Cmd.none )
 
+type Msg = PressKey String
 
-type Msg
-  = DraftChanged String
-  | Send
-  | Recv String
-
-
--- Use the `sendMessage` port when someone presses ENTER or clicks
--- the "Send" button. Check out index.html to see the corresponding
--- JS where this is piped into a WebSocket.
---
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    DraftChanged draft ->
-      ( { model | draft = draft }
-      , Cmd.none
-      )
-
-    Send ->
-      ( { model | draft = "" }
-      , sendMessage model.draft
-      )
-
-    Recv message ->
-      ( { model | messages = model.messages ++ [message] }
-      , Cmd.none
-      )
+    PressKey key ->
+      case key of
+        "ArrowUp" -> ({ model | direction = Up }, Cmd.none)
+        "ArrowDown" -> ({ model | direction = Down }, Cmd.none)
+        "ArrowLeft" -> ({ model | direction = Left }, Cmd.none)
+        "ArrowRight" -> ({ model | direction = Right }, Cmd.none)
+        _ -> (model, Cmd.none)
 
 
 
--- SUBSCRIPTIONS
-
-
--- Subscribe to the `messageReceiver` port to hear about messages coming in
--- from JS. Check out the index.html file to see how this is hooked up to a
--- WebSocket.
---
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-  messageReceiver Recv
+subscriptions model = Browser.Events.onKeyDown (Json.Decode.map PressKey keyDecoder)
 
-
-
--- VIEW
-
+keyDecoder : Json.Decode.Decoder String
+keyDecoder =
+    Json.Decode.field "key" Json.Decode.string
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text "Echo Chat" ]
-    , ul []
-        (List.map (\msg -> li [] [ text msg ]) model.messages)
-    , input
-        [ type_ "text"
-        , placeholder "Draft"
-        , onInput DraftChanged
-        , on "keydown" (ifIsEnter Send)
-        , value model.draft
-        ]
-        []
-    , button [ onClick Send ] [ text "Send" ]
-    ]
-
-
-
--- DETECT ENTER
-
-
-ifIsEnter : msg -> D.Decoder msg
-ifIsEnter msg =
-  D.field "key" D.string
-    |> D.andThen (\key -> if key == "Enter" then D.succeed msg else D.fail "some other key")
+    div [ class "container mx-auto h-screen place-content-center" ] [
+      div [class ("bg-blue-500 mx-auto w-[" ++ String.fromInt model.area ++ "px] h-[" ++ String.fromInt model.area ++ "px]")  ] []
+     ]
