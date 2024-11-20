e22dddc05ede3c5558d4d69120c03ec5c73aa693
Angel Eduardo <40308240+Anedu91@users.noreply.github.com>
2024-11-20 16:42:51 -0300

    click event

--- front/src/Main.elm
+++ front/src/Main.elm
@@ -4,6 +4,7 @@ import Browser.Events
 
 
 import Html exposing (Html, div)
+import Html.Events exposing (on)
 import Html.Attributes exposing (class)
 import Json.Decode
 
@@ -13,7 +14,8 @@ main =
 type alias Model = {
   snakePosition: (Int, Int),
   area: Int,
-  direction: Direction
+  direction: Direction,
+  foodPosition: (Int, Int)
   }
 
 type Direction = Up | Down | Left | Right
@@ -21,9 +23,9 @@ type Direction = Up | Down | Left | Right
 
 
 init: () -> (Model, Cmd Msg)
-init _ = ( { snakePosition = (0,0), area = 500, direction = Up }, Cmd.none )
+init _ = ( { snakePosition = (0,0), area = 500, direction = Up, foodPosition = (0,0) }, Cmd.none )
 
-type Msg = PressKey String
+type Msg = PressKey String | ClickedPosition (Int, Int)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -35,8 +37,8 @@ update msg model =
         "ArrowLeft" -> (moveSnake { model | direction = Left }, Cmd.none)
         "ArrowRight" -> (moveSnake { model | direction = Right }, Cmd.none)
         _ -> (model, Cmd.none)
-
-
+    ClickedPosition (x, y) ->
+      ({ model | foodPosition = (x, y)}, Cmd.none)
 moveSnake : Model -> Model
 moveSnake model =
   let
@@ -50,17 +52,22 @@ moveSnake model =
   {model | snakePosition = newPosition}
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Browser.Events.onKeyDown (Json.Decode.map PressKey keyDecoder)
+subscriptions _ = Browser.Events.onKeyDown (Json.Decode.map PressKey keyDecoder)
 
 keyDecoder : Json.Decode.Decoder String
 keyDecoder =
     Json.Decode.field "key" Json.Decode.string
 
+clickDecoder : Json.Decode.Decoder Msg
+clickDecoder =
+  Json.Decode.map2 Tuple.pair (Json.Decode.field "offsetX" Json.Decode.int) (Json.Decode.field "offsetY" Json.Decode.int) |> Json.Decode.map ClickedPosition
+
+
 view : Model -> Html Msg
 view model =
     div [ class "container mx-auto h-screen place-content-center" ] [
-      div [class ("bg-blue-500 mx-auto relative w-[" ++ String.fromInt model.area ++ "px] h-[" ++ String.fromInt model.area ++ "px]")  ] [
-        div [class ("absolute top-[" ++ String.fromInt (Tuple.second model.snakePosition) ++ "px] left-[" ++ String.fromInt (Tuple.first model.snakePosition) ++ "px] w-10 h-10 bg-red-500") ] []
+      div [class ("bg-blue-500 mx-auto relative w-[" ++ String.fromInt model.area ++ "px] h-[" ++ String.fromInt model.area ++ "px]"), Html.Events.on "click" clickDecoder   ] [
+        div [class ("absolute top-[" ++ String.fromInt (Tuple.second model.snakePosition) ++ "px] left-[" ++ String.fromInt (Tuple.first model.snakePosition) ++ "px] w-10 h-10 bg-red-500") ] [],
+        div [class ("absolute top-[" ++ String.fromInt (Tuple.second model.foodPosition) ++ "px] left-[" ++ String.fromInt (Tuple.first model.foodPosition) ++ "px] w-3 h-3 bg-green-500") ] []
       ]
-
      ]
